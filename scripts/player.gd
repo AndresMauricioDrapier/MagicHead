0fba5ffc73734d54f45a4506048121690297ab1e
Satiring <escuelaps@gmail.com>
2024-05-18 00:05:48 +0200

    Improve Player Scene and script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,29 @@
 extends CharacterBody2D
 
+enum DIRECTION {LEFT, RIGHT, UP, DOWN}
 
-const SPEED = 100
+var speed = 100  # move speed in pixels/sec
 
-func get_input():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_direction * SPEED
+func get_input(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	check_sprite_direction(direction.x)
+	velocity = direction * speed
+	
+func check_sprite_direction(actualDirectionFacing: float):
+	if actualDirectionFacing < 0:
+		set_sprite_direction(DIRECTION.LEFT)
+	else:
+		set_sprite_direction(DIRECTION.RIGHT)
+		
+func set_sprite_direction(newDirection: DIRECTION):
+	if newDirection == DIRECTION.RIGHT && $Sprite.is_flipped_h():
+		$Sprite.set_flip_h(false)
+		
+	if newDirection == DIRECTION.LEFT && !$Sprite.is_flipped_h():
+		$Sprite.set_flip_h(true)
+		
+	return
 
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
 	move_and_slide()
